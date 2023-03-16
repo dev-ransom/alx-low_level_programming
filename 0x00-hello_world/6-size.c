03f5d0b1e33c9eb741daea36a13290105ff1e14f
Sunday Ransom <sundayyoung@gmail.com>
2023-03-16 15:52:54 -0700

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - this is main function
+ * main - Entry points
  *
  * Return: 0 when successful
  */
-int main(void)
 
+int main(void)
 {
-	printf("Size of a char: %d bytes(s)\n", sizeof(char));
-	printf("size of a int: %d bytes(s)\n", sizeof(int));
+	printf("size of a char: %d bytes(s)\n", sizeof(char));
+	printf("size of an int: %d bytes(s)\n", sizeof(int));
 	printf("size of a long int: %d bytes(s)\n", sizeof(long int));
 	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("size of a float: %d bytes(s)\n", sizeof(float));
