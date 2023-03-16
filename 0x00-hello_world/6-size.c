2a0122a5451ab1d24d050bf66d30d8871adc2bbe
Sunday Ransom <sundayyoung@gmail.com>
2023-03-16 14:38:43 -0700

    size of files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,12 @@
  * Return: 0 when successful
  */
 int main(void)
+
 {
-	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
-	printf("size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("size of a int: %d bytes(s)\n", sizeof(int));
+	printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
