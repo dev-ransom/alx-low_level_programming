2539b7874afc152fd24ae3fec63a6208197df00d
Sunday Ransom <sundayyoung@gmail.com>
2023-03-16 17:26:25 -0700

    added modification on 6-files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main Entry point
+ * main - Entry point
  *
- * Result: 0 always when successful
+ * Result: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("size of a char: %d bytes(s)\n", sizeof(char));
-	printf("size of an int: %d bytes(s)\n", sizeof(int));
-	printf("size of a long int: %d bytes(s)\n", sizeof(long int));
-	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %d bytes(s)\n", sizeof(float));
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
