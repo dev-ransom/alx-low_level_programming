a1d4a1ef35b7662d569029df2e7dcc51df96313d
Sunday Ransom <sundayyoung@gmail.com>
2023-03-23 16:05:03 -0700

    added code for 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,12 @@
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
