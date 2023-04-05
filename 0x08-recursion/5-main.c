b6ad0ff9054ed8200ee44556bb99f74823ffae80
Sunday Ransom <sundayyoung@gmail.com>
2023-04-05 01:36:06 -0700

    added all modifications on .c files

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -10,13 +10,17 @@ int main(void)
 {
     int r;
 
-    r = is_palindrome("level");
+    r = _sqrt_recursion(1);
     printf("%d\n", r);
-    r = is_palindrome("redder");
+    r = _sqrt_recursion(1024);
     printf("%d\n", r);
-    r = is_palindrome("test");
+    r = _sqrt_recursion(16);
     printf("%d\n", r);
-    r = is_palindrome("step on no pets");
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
     printf("%d\n", r);
     return (0);
 }
